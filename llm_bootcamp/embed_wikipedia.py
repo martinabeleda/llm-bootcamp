ceeb65f39096c11e046385558f43888aa0c78ca3
martinabeleda <martin.abeleda@gmail.com>
2023-11-08 13:10:26 -0800

    add wikipedia embedding code

--- llm_bootcamp/embed_wikipedia.py
+++ llm_bootcamp/embed_wikipedia.py
@@ -13,6 +13,8 @@ from loguru import logger
 import mwclient
 import mwparserfromhell
 from openai import OpenAI
+import pandas as pd
+import tiktoken
 
 
 client = OpenAI(
@@ -22,6 +24,10 @@ client = OpenAI(
 
 EMBEDDING_MODEL = "text-embedding-ada-002"
 GPT_MODEL = "gpt-4"
+
+BATCH_SIZE = 1000
+
+SAVE_PATH = "data/winter_olympics_2022.csv"
 
 CATEGORY_TITLE = "Category:2022 Winter Olympics"
 WIKI_SITE = "en.wikipedia.org"
@@ -143,6 +149,97 @@ def keep_section(section: tuple[list[str], str]) -> bool:
         return True
 
 
+def num_tokens(text: str, model: str = GPT_MODEL) -> int:
+    """Return the number of tokens in a string."""
+    encoding = tiktoken.encoding_for_model(model)
+    return len(encoding.encode(text))
+
+
+def halved_by_delimiter(string: str, delimiter: str = "\n") -> list[str, str]:
+    """Split a string in two, on a delimiter, trying to balance tokens on each side."""
+    chunks = string.split(delimiter)
+    if len(chunks) == 1:
+        return [string, ""]  # no delimiter found
+    elif len(chunks) == 2:
+        return chunks  # no need to search for halfway point
+    else:
+        total_tokens = num_tokens(string)
+        halfway = total_tokens // 2
+        best_diff = halfway
+        for i, chunk in enumerate(chunks):
+            left = delimiter.join(chunks[: i + 1])
+            left_tokens = num_tokens(left)
+            diff = abs(halfway - left_tokens)
+            if diff >= best_diff:
+                break
+            else:
+                best_diff = diff
+        left = delimiter.join(chunks[:i])
+        right = delimiter.join(chunks[i:])
+        return [left, right]
+
+
+def truncated_string(
+    string: str,
+    model: str,
+    max_tokens: int,
+    print_warning: bool = True,
+) -> str:
+    """Truncate a string to a maximum number of tokens."""
+    encoding = tiktoken.encoding_for_model(model)
+    encoded_string = encoding.encode(string)
+    truncated_string = encoding.decode(encoded_string[:max_tokens])
+    if print_warning and len(encoded_string) > max_tokens:
+        logger.warning(
+            f"Truncated string from {len(encoded_string)} tokens to {max_tokens} tokens."
+        )
+    return truncated_string
+
+
+def split_strings_from_subsection(
+    subsection: tuple[list[str], str],
+    max_tokens: int = 1000,
+    model: str = GPT_MODEL,
+    max_recursion: int = 5,
+) -> list[str]:
+    """
+    Split a subsection into a list of subsections, each with no more than max_tokens.
+    Each subsection is a tuple of parent titles [H1, H2, ...] and text (str).
+    """
+    titles, text = subsection
+    string = "\n\n".join(titles + [text])
+    num_tokens_in_string = num_tokens(string)
+    # if length is fine, return string
+    if num_tokens_in_string <= max_tokens:
+        return [string]
+    # if recursion hasn't found a split after X iterations, just truncate
+    elif max_recursion == 0:
+        return [truncated_string(string, model=model, max_tokens=max_tokens)]
+    # otherwise, split in half and recurse
+    else:
+        titles, text = subsection
+        for delimiter in ["\n\n", "\n", ". "]:
+            left, right = halved_by_delimiter(text, delimiter=delimiter)
+            if left == "" or right == "":
+                # if either half is empty, retry with a more fine-grained delimiter
+                continue
+            else:
+                # recurse on each half
+                results = []
+                for half in [left, right]:
+                    half_subsection = (titles, half)
+                    half_strings = split_strings_from_subsection(
+                        half_subsection,
+                        max_tokens=max_tokens,
+                        model=model,
+                        max_recursion=max_recursion - 1,
+                    )
+                    results.extend(half_strings)
+                return results
+    # otherwise no split was found, so just truncate (should be very rare)
+    return [truncated_string(string, model=model, max_tokens=max_tokens)]
+
+
 def main():
     site = mwclient.Site(WIKI_SITE)
     category_page = site.pages[CATEGORY_TITLE]
@@ -163,3 +260,35 @@ def main():
 
     for ws in wikipedia_sections[:5]:
         logger.info(f"{ws[0]} {ws[1][:77]}...")
+
+    # split sections into chunks
+    MAX_TOKENS = 1600
+    wikipedia_strings = []
+    for section in wikipedia_sections:
+        wikipedia_strings.extend(
+            split_strings_from_subsection(section, max_tokens=MAX_TOKENS)
+        )
+
+    logger.info(
+        f"{len(wikipedia_sections)} Wikipedia sections split into {len(wikipedia_strings)} strings."
+    )
+    logger.info(wikipedia_strings[1])
+
+    embeddings = []
+    for batch_start in range(0, len(wikipedia_strings), BATCH_SIZE):
+        batch_end = batch_start + BATCH_SIZE
+        batch = wikipedia_strings[batch_start:batch_end]
+        logger.info(f"Batch {batch_start} to {batch_end-1}")
+        response = client.embeddings.create(model=EMBEDDING_MODEL, input=batch)
+        for i, be in enumerate(response.data):
+            assert i == be.index
+        batch_embeddings = [e.embedding for e in response.data]
+        embeddings.extend(batch_embeddings)
+
+    df = pd.DataFrame({"text": wikipedia_strings, "embedding": embeddings})
+
+    df.to_csv(SAVE_PATH, index=False)
+
+
+if __name__ == "__main__":
+    main()
